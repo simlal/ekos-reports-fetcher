8fb37cb15dcfdfbb59a43b600e688265b242de39
Simon Lalonde <simlalonde@hotmail.com>
2023-10-13 17:58:54 -0400

    Complete working automated report fetching

--- scripts/fetch_ekos.js
+++ scripts/fetch_ekos.js
@@ -1,6 +1,8 @@
 const puppeteer = require("puppeteer");
 const dotenv = require("dotenv");
 const path = require("path")
+const fs = require('fs');
+
 
 const dotenvFile = path.join("..", ".env")
 dotenv.config({path: dotenvFile})
@@ -13,19 +15,46 @@ const pptrLaunchParams = {
     viewPort: {
         width: 1080,
         height: 1024
-    }
+    },
+    downloadTimeout: 5000
 }
-const downloadPath = path.resolve(__dirname)
+const savePath = path.join(__dirname, "..", "data")
 
 // Ekos related data
 const ekosLoginUrl = "https://login.goekos.com/";
-const brewInfoReportName = "CQ data - brewInfo";
-const ingredientsReportName = "CQ - Ingredients (completed)";
-const fermentationReportName = "CQ data - Fermentation (all-progress)";
+
+const reportNameQuerries = [
+    "CQ data - brewInfo",
+    "CQ - Ingredients (completed)",
+    "CQ data - Fermentation (all-progress)"
+]
+const newReportFileNames = ["brewinfo.csv", "ingredients.csv", "fermentation.csv"]
 
 // Helper for deprecated waitForTimeout
 async function pageTimeout(ms) {
-    new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Rename report file once downloaded
+function renameDownload(savePath, newName) {
+    fs.readdir(savePath, (err, files) => {
+        if (err) throw err;
+        
+        for (const file of files) {
+            // Delete exising file with target name
+            if (file === newName) {
+                fs.unlink(path.join(savePath, file), err => {
+                    if (err) throw err;
+                });
+            }
+            // Rename the newly download file
+            if (path.extname(file) === ".csv" && !newReportFileNames.includes(file)) {
+                fs.rename(path.join(savePath, file), path.join(savePath, newName), err => {
+                    if (err) throw err;
+                });
+            }
+        }
+    });
 }
 
 // Main function to fetch Ekos data
@@ -33,6 +62,14 @@ async function fetchEkosData(launchParams) {
     // Launch browser
     const browser = await puppeteer.launch(launchParams);
     const page = await browser.newPage();
+
+    // Set download path
+    const client = await page.target().createCDPSession();
+    await client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: savePath
+    });
+
 
     // navigate to Ekos login page and set viewPort
     await page.goto(ekosLoginUrl);
@@ -52,39 +89,42 @@ async function fetchEkosData(launchParams) {
         .then(button => button.click());
     
     // *** Fetch brewInfo report ***
-    // Type in report name and naviguate to report page
-    await page.waitForSelector("input.sc-uVWWZ.jjluET.sc-Nxspf.dXYREZ.ReportsList__StyledSearchInput-reports-ui__sc-wzkmmj-2.iBbXOq");
-    await page.type(
-        "input.sc-uVWWZ.jjluET.sc-Nxspf.dXYREZ.ReportsList__StyledSearchInput-reports-ui__sc-wzkmmj-2.iBbXOq", 
-        brewInfoReportName,
-        {delay: 1});
+    for (let i = 0; i < reportNameQuerries.length; i++) {
+        let reportName = reportNameQuerries[i];
+        // Type in report name and naviguate to report page
+        await page.waitForSelector("input.sc-uVWWZ.jjluET.sc-Nxspf.dXYREZ.ReportsList__StyledSearchInput-reports-ui__sc-wzkmmj-2.iBbXOq");
+        await page.type(
+            "input.sc-uVWWZ.jjluET.sc-Nxspf.dXYREZ.ReportsList__StyledSearchInput-reports-ui__sc-wzkmmj-2.iBbXOq", 
+            reportName,
+            {delay: 1});
 
-    await page.waitForSelector(".Link-reports-ui__sc-81gbfs-0")
-        .then(a => a.click());
+        await page.waitForSelector(".Link-reports-ui__sc-81gbfs-0")
+            .then(a => a.click());
 
-    // Wait for loading indicator to finish
-    await page.waitForSelector(".loading-indicator").then(console.log("loaded ind"));
-    await page.waitForSelector(".loading-indicator", {hidden: true}).then(console.log("loaded ind DONE!"));
+        // Wait for loading indicator to finish
+        await page.waitForSelector(".loading-indicator");
+        await page.waitForSelector(".loading-indicator", {hidden: true});
 
-    // Get the report body which is nested inside iframe
-    let iframe = await page.waitForSelector("iframe").then(iframe => iframe.contentFrame());
-    
-    await iframe.waitForSelector(".button_section");
-    await iframe.waitForSelector(".buttonGroupOuter")
-        .then(button => button.click());
+        // Get the report body which is nested inside iframe
+        let iframe = await page.waitForSelector("iframe").then(iframe => iframe.contentFrame());
+        
+        await iframe.waitForSelector(".button_section");
+        await iframe.waitForSelector(".buttonGroupOuter")
+            .then(button => button.click());
 
-    // Download the file and save it locally
-    // const fs = require('fs');
-    // fs.watch(downloadPath, (eventType, filename) => {
-    //     if (eventType === 'rename' && filename) {
-    //       console.log(`File ${filename} was added to the directory`);
-    //       // File has been downloaded, you can perform the next action here
-    //     }
-    //   });
-    
-    await iframe.waitForSelector(".buttonGroupOptions")
-    await iframe.waitForSelector("#csv_export")
-        .then(button => button.click());
+        // Download the report and rename it
+            await iframe.waitForSelector(".buttonGroupOptions")
+        await iframe.waitForSelector("#csv_export")
+            .then(button => button.click())
+            .then(console.log("Started downloading report: " + reportName));
+        await pageTimeout(launchParams.downloadTimeout)    // No puppeteer wait for download to finish, so we wait 5s
+            .then(console.log(`Finished ${launchParams.downloadTimeout} timeout for download.`))
+        // Rename the file
+        renameDownload(savePath, newReportFileNames[i]);
+        console.log("Renamed report to " + newReportFileNames[i]);
+        await iframe.waitForSelector(".CloseButton")
+            .then(button => button.click());
+    }
 
     await browser.close();
 }
